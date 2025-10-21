ee92b6ff1a2619d0922d70da96d7a4c65a6bce53
bianca alencar <alencar-bianca@hotmail.com>
2025-10-21 20:28:18 -0300

    menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,36 @@
-import './menu.css';
 import { CiInstagram } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
+
+import './menu.css';
+import logo from '../../assets/imgs/logo/logopng.png';
+
 
 export default function Menu() { 
   return (
-    <nav className="bg-yellow-500 py-2">
-      <div className="content flex justify-end items-center gap-3 pr-4">
-        <a href="#" target='_blank'>
-          <CiInstagram size={25} className="text-blue-900 hover:text-green-700 transition" />
-        </a>
-        <button className='cursor-pointer btn'>
-          Entre em contato
-        </button>
+    <header>
+      <div className="bg-primary py-2">
+        <ul className="content flex justify-end items-center gap-3 pr-4">
+          <li>
+            <a href="#" target='_blank'>
+              <CiInstagram size={25} className="text-secondary hover:text-green-700 transition" />
+            </a>
+          </li>
+          <li>
+            <button className='cursor-pointer btn '>
+              Entre em contato
+            </button>
+          </li>
+        </ul>
       </div>
-    </nav>
+      <nav className="content flex  items-center gap-3 pr-4">
+        <Link to={'/'}>
+        <img className='w-[160px]' src={logo} alt="Logo Servil" />
+        </Link>
+        <ul className="flex gap-6 text-lg font-medium justify-center w-full">
+          <li className='hover:text-accent'><Link to={'/'}>Home</Link></li>
+          <li className='hover:text-accent'><Link to={'/'}>Contato</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
