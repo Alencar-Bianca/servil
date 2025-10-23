87e706ce955903b2856b26cb3b49c70d1a991837
bianca alencar <alencar-bianca@hotmail.com>
2025-10-23 18:03:35 -0300

    solucoes

---
+++ src/components/Solution/Solution.jsx
@@ -0,0 +1,18 @@
+export default function Solution() {
+  return (
+    <section className="py-10 bg-primary text-white">
+      <div className="content max-w-3xl mx-auto">
+        <div className="flex justify-between items-center gap-6 flex-wrap">
+          <h2 className="font-bold text-3xl mb-2 flex-1">
+            Soluções <span className="text-secondary text-base font-normal">(sob medida para o seu negócio)</span>
+          </h2>
+          <p className="text-gray-100 mt-3 leading-relaxed text-lg flex-1">
+            Nossos equipamentos e sistemas oferecem automação e controle completos,
+            otimizando processos e elevando o desempenho operacional.
+            Mais eficiência, qualidade e confiabilidade em cada resultado.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Menu from "../../components/Menu/Menu";
 import OurWork from "../../components/OurWork/OurWork";
+import Solution from "../../components/Solution/Solution";
 
 export default function Home() {
   return (
     <>
       <Menu />
       <OurWork />
+      <Solution />
     </>
   );
 }
