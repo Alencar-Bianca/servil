92ddeadc88af06dc280aa982233469db2a449a67
bianca alencar <alencar-bianca@hotmail.com>
2025-10-23 18:08:17 -0300

    ajueste solucoes

--- src/components/Solution/Solution.jsx
+++ src/components/Solution/Solution.jsx
@@ -1,12 +1,12 @@
 export default function Solution() {
   return (
     <section className="py-10 bg-primary text-white">
-      <div className="content max-w-3xl mx-auto">
-        <div className="flex  items-center gap-6 flex-wrap justify-between flex-col md:flex-row">
-          <h2 className="font-bold text-3xl mb-2 flex-1">
+      <div className="content max-w-3xl mx-auto text-sm">
+        <div className="flex items-center gap-6 flex-wrap justify-between flex-col md:flex-row">
+          <h2 className="font-bold mb-2 flex-1 text-lg ">
             Soluções <span className="text-secondary text-base font-normal">(sob medida para o seu negócio)</span>
           </h2>
-          <p className="text-gray-100 mt-3 leading-relaxed text-lg flex-1">
+          <p className="text-gray-100 mt-3 leading-relaxed text-sm flex-1">
             Nossos equipamentos e sistemas oferecem automação e controle completos,
             otimizando processos e elevando o desempenho operacional.
             Mais eficiência, qualidade e confiabilidade em cada resultado.
